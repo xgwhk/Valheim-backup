a33b6a1678b627b1f5d2252e051b328b8bafe7a7
XinGeWen <xgw@hk-it-services.com>
2021-07-01 19:59:44 +0800

    Script file: modify dur to change of directory name

--- 00-copy-Valheim-config.sh
+++ 00-copy-Valheim-config.sh
@@ -1,13 +1,13 @@
  #!/bin/bash
 #reinitialise directories
-cd ~/Documents/git/Home/config-Valheim/config
+cd ~/Documents/git/Home/Valheim-backup/config
 rm -r *
-cd ~/Documents/git/Home/config-Valheim/plugins
+cd ~/Documents/git/Home/Valheim-backup/plugins
 rm -r *
-cd ~/Documents/git/Home/config-Valheim/patchers
+cd ~/Documents/git/Home/Valheim-backup/patchers
 rm -r *
 #copy file to git directories
-cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/config/* ~/Documents/git/Home/config-Valheim/config/.
-cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/plugins/* ~/Documents/git/Home/config-Valheim/plugins/.
-cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/patchers/* ~/Documents/git/Home/config-Valheim/patchers/.
+cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/config/* ~/Documents/git/Home/Valheim-backup/config/.
+cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/plugins/* ~/Documents/git/Home/Valheim-backup/plugins/.
+cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/patchers/* ~/Documents/git/Home/Valheim-backup/patchers/.
 
