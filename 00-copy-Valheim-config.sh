dcd31e4d47f5877e876b81528d48291530e90e12
XinGeWen <xgw@hk-it-services.com>
2021-07-01 11:46:02 +0800

    part 1 of the plugins

--- 00-copy-Valheim-config.sh
+++ 00-copy-Valheim-config.sh
@@ -1,3 +1,4 @@
  #!/bin/bash
-cp ~/Downloads/Valheim/000\ config/* ~/Documents/git/Home/config-Valheim/.
+cp -R ~/Downloads/Valheim/000\ config/* ~/Documents/git/Home/config-Valheim/.
+cp -R ~/Downloads/Valheim/000\ plugins/* ~/Documents/git/Home/config-Valheim/plugins/.
 
