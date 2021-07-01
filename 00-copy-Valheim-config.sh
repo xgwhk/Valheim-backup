fadd3ca562918de68ca3cba80f96855077cf8190
XinGeWen <xgw@hk-it-services.com>
2021-07-01 12:01:52 +0800

    adding the patchers folder

--- 00-copy-Valheim-config.sh
+++ 00-copy-Valheim-config.sh
@@ -1,4 +1,5 @@
  #!/bin/bash
-cp -R ~/Downloads/Valheim/000\ config/* ~/Documents/git/Home/config-Valheim/config/.
-cp -R ~/Downloads/Valheim/000\ plugins/* ~/Documents/git/Home/config-Valheim/plugins/.
+cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/config/* ~/Documents/git/Home/config-Valheim/config/.
+cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/plugins/* ~/Documents/git/Home/config-Valheim/plugins/.
+cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/patchers/* ~/Documents/git/Home/config-Valheim/patchers/.
 
