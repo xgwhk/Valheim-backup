8a3a8689e8fcc1f18f3f38a276ac0f1872421026
XinGeWen <xgw@hk-it-services.com>
2021-07-01 14:04:47 +0800

    Update the copy script to delete the directory content rior copying the data over

--- 00-copy-Valheim-config.sh
+++ 00-copy-Valheim-config.sh
@@ -1,4 +1,12 @@
  #!/bin/bash
+#reinitialise directories
+cd ~/Documents/git/Home/config-Valheim/config
+rm -r *
+cd ~/Documents/git/Home/config-Valheim/plugins
+rm -r *
+cd ~/Documents/git/Home/config-Valheim/patchers
+rm -r *
+#copy file to git directories
 cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/config/* ~/Documents/git/Home/config-Valheim/config/.
 cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/plugins/* ~/Documents/git/Home/config-Valheim/plugins/.
 cp -R ~/Downloads/Valheim/000\ Valheim/BepInEx/patchers/* ~/Documents/git/Home/config-Valheim/patchers/.
