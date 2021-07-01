9a5ae44941640279833b1b29011e53f56a91006e
XinGeWen <xgw@hk-it-services.com>
2021-07-01 11:53:59 +0800

    Reorganisation of the files

--- 00-copy-Valheim-config.sh
+++ 00-copy-Valheim-config.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
-cp -R ~/Downloads/Valheim/000\ config/* ~/Documents/git/Home/config-Valheim/.
+cp -R ~/Downloads/Valheim/000\ config/* ~/Documents/git/Home/config-Valheim/config/.
 cp -R ~/Downloads/Valheim/000\ plugins/* ~/Documents/git/Home/config-Valheim/plugins/.
 
